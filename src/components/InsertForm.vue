5beae280c869e91c4a0dff5dab6a11649b8909f3
tinlt2012 <106730003+tinlt2012@users.noreply.github.com>
2024-11-14 14:49:25 +0700

    Update InsertForm.vue

--- src/components/InsertForm.vue
+++ src/components/InsertForm.vue
@@ -20,7 +20,7 @@ export default {
   methods: {
     async submitForm() {
       try {
-        const response = await fetch('http://18.220.209.198:8080/insert', {
+        const response = await fetch('http://3.145.112.168/:8080/insert', {
           method: 'POST',
           headers: {
             'Content-Type': 'text/plain',
